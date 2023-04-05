1b2bdd9ceed8105119a19d85da7c415a4b9ef98b
Faz-R <FazaR92@gmail.com>
2023-04-06 02:57:23 +0300

    fix: fix test for search response

--- src/pages/HomePage/index.test.tsx
+++ src/pages/HomePage/index.test.tsx
@@ -7,10 +7,8 @@ import { BrowserRouter } from 'react-router-dom';
 describe('FormPage', () => {
   test('Use field search to find picture', async () => {
     render(<SearchPage />, { wrapper: BrowserRouter });
-    localStorage.setItem('search', '');
     await userEvent.type(screen.getByRole('searchbox'), 'Mone');
     await userEvent.click(screen.getByText(/Search/i));
-    const cardsArray = await screen.findAllByRole('listitem');
-    expect(cardsArray).toHaveLength(10);
+    expect(await screen.findAllByRole('listitem')).toHaveLength(10);
   });
 });
