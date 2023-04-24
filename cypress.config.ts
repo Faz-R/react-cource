78db9bb484182740499a536bdb958132345a934b
Faz-R <FazaR92@gmail.com>
2023-04-24 13:18:51 +0300

    reforge: remove comment

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,7 @@ import codeCoverage from '@cypress/code-coverage/task';
 
 export default defineConfig({
   e2e: {
-    // baseUrl: 'http://localhost:8000',
     video: false,
-    // supportFile: false,
     setupNodeEvents(on, config) {
       codeCoverage(on, config);
       return config;
